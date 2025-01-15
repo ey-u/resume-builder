aebdb7637c7cf9d9467a1b13c1e774fec9a87b5d
ey-u <175895848+ey-u@users.noreply.github.com>
2025-01-15 14:16:13 -0500

    Setup default data

---
+++ src/defaultData.js
@@ -0,0 +1,27 @@
+const defaultData = {
+  name: "Avatar Aang",
+  email: "aangis@thetemple.com",
+  phone: "333-333-3333",
+  location: "Southern Air Temple",
+  education: [
+    {
+      school: "Air Acolyte Academy",
+      location: "Southern Air Temple",
+      gradDate: "2008-07",
+      degree: "Master of Air, Airbending",
+    },
+  ],
+  experience: [
+    {
+      position: "Avatar",
+      company: "The Hundred Year War",
+      location: "Earth",
+      startDate: "2005-02",
+      endDate: "2008-07",
+      responsibilities:
+        "Mastered all four elements, defeated Firelord Ozai, ended Hundred Year War",
+    },
+  ],
+};
+
+export default defaultData;
