9510a2741eeb27515f424cd83d69b8c680938730
ey-u <175895848+ey-u@users.noreply.github.com>
2025-01-16 16:09:36 -0500

    Add more default data

--- src/defaultData.js
+++ src/defaultData.js
@@ -8,8 +8,15 @@ const defaultData = {
       id: crypto.randomUUID(),
       school: "Air Acolyte Academy",
       location: "Southern Air Temple",
-      gradDate: "2008-07",
+      gradDate: "2005-07",
       degree: "Master of Air, Airbending",
+    },
+    {
+      id: crypto.randomUUID(),
+      school: "Peacekeeper University",
+      location: "Earth",
+      gradDate: "2008-07",
+      degree: "Master of Peacekeeping, War Ender",
     },
   ],
   experience: [
@@ -21,7 +28,17 @@ const defaultData = {
       startDate: "2005-02",
       endDate: "2008-07",
       summary:
-        "Mastered all four elements; Defeated Firelord Ozai; Ended Hundred Year War",
+        "Mastered all four elements, lorem ipsum odor amet, consectetuer adipiscing elit; Defeated Firelord Ozai, vestibulum finibus interdum non interdum vulputate; Ended Hundred Year War, tristique consequat fusce at dis facilisi quam",
+    },
+    {
+      id: crypto.randomUUID(),
+      position: "Waterbending Student",
+      company: "The Katara School of Waterbending",
+      location: "Earth",
+      startDate: "2005-02",
+      endDate: "2005-12",
+      summary:
+        "Studied under Katara and mastered waterbending; Urna mattis donec curae ante quis, fringilla ex magna taciti proin accumsan eros consectetur habitasse; Dui sollicitudin porta mollis quis sociosqu laoreet finibus, est est ullamcorper tristique fringilla risus mauris; Lorem vehicula dictum elementum sapien, risus sociosqu nam tempus",
     },
   ],
 };
